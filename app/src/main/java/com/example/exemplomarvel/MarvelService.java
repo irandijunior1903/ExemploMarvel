57a470cc73ea282273619e66cacbb09f8cfa7658
Irandi Junior <irandi.machado@dcx.ufpb.br>
2021-10-09 09:45:53 -0300

    Atualização 2

--- app/src/main/java/com/example/exemplomarvel/MarvelService.java
+++ app/src/main/java/com/example/exemplomarvel/MarvelService.java
@@ -1,15 +1,14 @@
 package com.example.exemplomarvel;
 
 
-import com.example.exemplomarvel.models.Comic;
-
-import java.util.List;
+import com.example.exemplomarvel.models.ComicDataContainer;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface MarvelService {
 
-    @GET
-    Call<List<Comic>> getAllComics();
+    @GET("comics?ts=1&apikey=f037ab6e62e83f9bbd49c158cbb0b541&hash=4dd1d9efdd156d320064a627542547c9")
+    Call<ComicDataContainer> getAllComics(@Query("results")int size);
 }
